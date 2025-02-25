10adfcb8b4f027ceca67870a78cb07bbcc9306e6
sheabali <iamskpranto@gmail.com>
2025-02-26 01:30:03 +0600

    Skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from '@/pages/Home/Banner/Banner';
+import Skills from '@/pages/Home/Skills/Skills';
 
 export default function Home() {
   return (
     <div className="">
       <Banner />
+      <Skills />
     </div>
   );
 }
